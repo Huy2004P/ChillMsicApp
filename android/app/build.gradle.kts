9c6b4268182206011e266291769fe702cf821f1b
Văn Bá Phát Huy <phathuy2004h@gmail.com>
2026-06-14 12:34:11 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.music_app"
+    namespace = "com.vanbaphathuy.chillmsic"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.music_app"
+        applicationId = "com.vanbaphathuy.chillmsic"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
